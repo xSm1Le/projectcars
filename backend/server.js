7cfcee0f259e40ee9a415d529abf7dbc323c52d9
WebDevDB <db.dci.student@gmail.com>
2024-02-13 00:36:57 +0100

    add comments

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-import express from 'express'
+import express from 'express'  
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import userRouter from './src/routes/userRoutes.js'
 
-dotenv.config()
+dotenv.config() // Zugriff auf .env-Datei
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express(); // Express-App
+const port = process.env.PORT || 5000; // Port
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // CORS
+app.use(express.json()); // JSON-Parser
 
 // Datenbankverbindung
 mongoose.connect(process.env.MONGODB_URI, {
   
 }).then(() => console.log('MongoDB Verbindung erfolgreich.'))
-  .catch(err => console.error('MongoDB Verbindung fehlgeschlagen: ', err));
+  .catch(err => console.error('MongoDB Verbindung fehlgeschlagen: ', err)); // MongoDB-Verbindung herstellen
 
 
-app.use('/api/users', userRouter)
+app.use('/api/users', userRouter) // User-Router
 
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`)  // Server starten
 });
