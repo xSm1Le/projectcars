03ced181ab22383bcdcfeb2ce65ced28aa90dac5
WebDevDB <db.dci.student@gmail.com>
2024-02-12 20:28:03 +0100

    mongodb conection debuging

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import userRouter from './src/routes/userRoutes.js'
 
 dotenv.config()
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log(err))
+// Datenbankverbindung
+mongoose.connect(process.env.MONGODB_URI, {
+  
+}).then(() => console.log('MongoDB Verbindung erfolgreich.'))
+  .catch(err => console.error('MongoDB Verbindung fehlgeschlagen: ', err));
+
 
 app.use('/api/users', userRouter)
 
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
-})
+});
