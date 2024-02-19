18c564e39c5579404370110de674b1323fa885c0
Bastian Meese <Bastian.Meese@web.de>
2024-02-19 13:19:50 +0100

    Kleinere Bugs gefixt

--- frontend/src/components/global/router.jsx
+++ frontend/src/components/global/router.jsx
@@ -19,7 +19,7 @@ export const router = createBrowserRouter([
             {
                 path: "login", 
                 element: <Login />
-            }
+            },
         ],
     },
 ]);
