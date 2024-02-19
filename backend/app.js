598b6b1cbc28bcb345172ee7a123b2ce83e50f51
WebDevDB <db.dci.student@gmail.com>
2024-02-19 10:37:32 +0100

    create indexrouter

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,28 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './src/config/db.js'
+import routes from './src/routes/indexRoute.js'
 
 
 dotenv.config()
 
 connectDB()
 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('API is running....')
+}
+);
+
+app.use('/api', routes)
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// Path: backend/src/config/db.js
---
+++ backend/src/routes/indexRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import userRoutes from './userRoutes.js';
+
+
+const router = express.Router();
+
+router.use('/users', userRoutes);
+
+export default router;
+
+// Path: backend/src/routes/userRoutes.js
