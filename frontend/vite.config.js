a443b17673d9f5b2fbd63b247010b897c9d1982a
MariaV96 <maria.vogel96@gmail.com>
2024-02-19 23:33:00 +0100

    Grobes PWA Gerüst gebaut

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugin = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.icon', 'apple-touc-icon.png', 'masked-icon.png'],
+  manifest: {
+    name: 'Project Cars',
+    short_name: 'Project Cars',
+    description: 'eine Fahrzeugverwaltungsapp',
+    icons: [
+      {
+        src: './pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: './pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)]
 })
