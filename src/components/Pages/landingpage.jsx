fa856154472f8e80ee96eb71c5166696911764ee
Bastian Meese <Bastian.Meese@web.de>
2024-02-21 09:59:32 +0100

    Landingpage navbar hinzugefügt

--- src/components/Pages/landingpage.jsx
+++ src/components/Pages/landingpage.jsx
@@ -1,8 +1,9 @@
-
+import { Navbar } from '../reusables/nav';
 
 export const Landingpage = () => {
     return (
         <section>
+            <Navbar />
             <h1>Landingpage</h1>
         </section>
     )
