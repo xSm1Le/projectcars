8bb7fdfd15c22c3a6e76b5b8fd1035b7442fd98e
Bastian Meese <Bastian.Meese@web.de>
2024-03-14 11:21:58 +0100

    function von den Icons bearbeitet

---
+++ src/components/Pages/Forms/carForm.jsx
@@ -0,0 +1,23 @@
+export const CarForm = ({cartype}) => {
+
+
+    return (
+        <form className="angaben" onSubmit={(e) => formFunction(e,cartype)}>
+            <input type="text" placeholder="Kennzeichen" />
+            <input type="text" placeholder="Marke" />
+            <input type="text" placeholder="Modell" />
+            <input type="text" placeholder="Kraftstoff" />
+            <input type="text" placeholder="Schadstoffklasse" />
+            <input type="text" placeholder="Leistung kW" />
+            <input type="text" placeholder="Leistung PS" />
+            <input type="text" placeholder="Kilometerstand" />
+            <input type="text" placeholder="Letzter TÜV Termin" />
+            <input type="text" placeholder="Letzter Ölwechsel" />
+            <input type="text" placeholder="Nächster Ölwechsel Nach Kilometern" />
+            <input type="text" placeholder="Datum Nächster Ölwechsel" />
+            <input type="text" placeholder="letzter Service" />
+            <button>Fahrzeug Speichern</button>
+        </form>
+        
+    )
+}
---
+++ src/components/Pages/Forms/trailerForm.jsx
@@ -0,0 +1,16 @@
+export const TrailerForm = ({cartype}) => {
+
+
+    return (
+        <form className="angaben" onSubmit={(e) => formFunction(e,cartype)}>
+            <input type="text" placeholder="Kennzeichen" />
+            <input type="text" placeholder="Marke" />
+            <input type="text" placeholder="Modell" />
+            <input type="text" placeholder="Max KM/H" />
+            <input type="text" placeholder="Volumen / Größe" />
+            <input type="text" placeholder="Letzter TÜV Termin" />
+            <input type="text" placeholder="letzter Service" />
+            <button>Fahrzeug Speichern</button>
+        </form>
+    )
+}
