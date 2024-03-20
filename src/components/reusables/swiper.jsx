5070729bb57d056b94fe04cf488204735f633a0a
MariaV96 <maria.vogel96@gmail.com>
2024-03-20 09:01:10 +0100

    kalender und landingpage

--- src/components/reusables/swiper.jsx
+++ src/components/reusables/swiper.jsx
@@ -22,25 +22,25 @@ export const SlideShow =  () => {
       >
         <SwiperSlide>
             <div className='slideDiv' >
-                <h3>BSP VW Passat B8</h3>
-{/*                  <img src="../beispielAutos/VW_Passat_B8_Limousine_2.0_TDI_Highline.JPG" alt="auto" />  */}
+                <h3 className='autoname'>BSP VW Passat B8</h3>
+                  <img src="../beispielAutos/VW_Passat_B8_Limousine_2.0_TDI_Highline.JPG" alt="auto" />
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slideDiv'>
-                <h3>BSP Audi Q7</h3>
+                <h3 className='autoname'>BSP Audi Q7</h3>
                 <img src="../beispielAutos/Audi_Q7.jpg" alt="auto" />
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slideDiv'>
-                <h3>BSP BMW E90</h3>
+                <h3 className='autoname'>BSP BMW E90</h3>
                 <img src="../beispielAutos/BMW_3er_E90.jpg" alt="auto" />
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slideDiv'>
-                <h3>BSP Skoda superb</h3>
+                <h3 className='autoname'>BSP Skoda superb</h3>
                 <img src="../beispielAutos/1200px-2019_Skoda_Superb_SE_L_Executive_TDi_2.0_Front.jpg" alt="auto" />
             </div>
         </SwiperSlide>
